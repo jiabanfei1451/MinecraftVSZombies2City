0a70cd53e4497646dfa4e93681920886a62044b1
痊愈加班费-quanyvjiabanfei <2200255183@qq.com>
2026-08-18 19:28:24 +0800

    Add Zombies Animation

--- .godot/mono/temp/obj/Debug/MinecraftVSZombies2 City.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/MinecraftVSZombies2 City.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftVSZombies2 City")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705a97da3caed8496bc6f8a509243fab9f36d59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4634ee0541d662b1fe893a6650400b325f7e8a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftVSZombies2 City")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftVSZombies2 City")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
